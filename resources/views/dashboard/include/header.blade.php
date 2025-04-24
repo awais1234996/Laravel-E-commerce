9603740d21dab563fc4cc6094b6682e13e40d0be
Awais Raza <awaisraza031074@gmail.com>
2025-04-24 12:50:00 +0500

    Okay

--- resources/views/dashboard/include/header.blade.php
+++ resources/views/dashboard/include/header.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/tr.png') }}">
-    <title>ECommy</title>
+    <title>E-Commerce</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
